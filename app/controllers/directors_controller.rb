ead6fef0113e5462b48155731980b92ce28dcbd9
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-27 12:54:21 +0300

    done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,18 +3,12 @@ class DirectorsController < ApplicationController
 
   def index
     directors = Director.all
-    render json: directors
+    render json: directors, include: ['movies', 'movies.reviews']
   end
 
   def show
     director = Director.find(params[:id])
-    render json: director
-  end
-
-  private
-
-  def render_not_found_response
-    render json: { error: "Director not found" }, status: :not_found
+    render json: director, include: ['movies', 'movies.reviews']
   end
 
 end
